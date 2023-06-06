04b9162f128a7e54b8d5e1dff579005052c58dfb
Asha <devarasettyasha@gmail.com>
2023-06-06 12:02:27 +0530

    eclise to git hub code psuh

---
+++ src/eclipsegitpractice/test.java
@@ -0,0 +1,17 @@
+package eclipsegitpractice;
+
+public class test {
+	
+	public void test()
+	{
+		System.out.println("test class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		test t = new test();
+		t.test();
+	}
+
+}
